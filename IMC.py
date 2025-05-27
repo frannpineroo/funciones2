158ed77507abe1e96a247d6ad23834f626929437
frannpineroo <frannpinero@hotmail.com>
2025-05-27 16:11:27 -0300

    cambios en el ej nro 4 aniadiendo feat, inch lb

--- IMC.py
+++ IMC.py
@@ -1,4 +1,13 @@
-def imc(weight, height):
+def featInchToMeters(ft, inch = 0.0):
+    return ft * 0.3048 + inch * 0.0254
+
+def lbToKg(lb):
+    return lb * 0.4535923
+
+def cmi(weight, height):
+    if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
+        return None
+
     return weight / height ** 2
 
-print(imc(52.5, 1.65))
+print(cmi(weight = lbToKg(176), height = featInchToMeters(5, 7)))
